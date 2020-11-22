e82f5c8a920e2ff8ab0a85bdff20f8677dad4bd7
Leon <hq_li1982@hotmail.com>
2020-11-22 19:56:49 +0800

    add node_modules

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,22 @@
-const mysql = require('mysql');
+var mysql = require('mysql');
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: '123456',
+        database: 'burgers_db'
+    });
+}
 
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '123456',
-    database: 'burgers_db'
-});
+
+
+
 
 connection.connect(function (err) {
     if (err) {
